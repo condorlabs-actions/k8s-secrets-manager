c3af1fea29845790fce262360b6b0b29dab27129
FlavioAandres <fpareja@condorlabs.io>
2021-07-15 08:04:48 -0500

    last changes and matchbox config files

--- index.js
+++ index.js
@@ -1,69 +1,59 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const awsHelper = require('./src/aws-helpers')
+const awsHelper = require('./src/aws-helpers');
 const YAML = require('json-to-pretty-yaml');
+const fs = require('fs');
 
-// const fs = require('fs');
-// const json = require('input.json');
- 
-// const data = YAML.stringify(json);
-// fs.writeFile('output.yaml', data);
-
-const run = async ()=>{
-  let secretCollections = []
+const run = async () => {
+  const secretCollections = [];
   try {
-    let AwsSecretsConfigs; 
+    let AwsSecretsConfigs;
     try {
-      AwsSecretsConfigs = JSON.parse(core.getInput("eks_secrets_configs")); 
-      console.log(JSON.stringify(AwsSecretsConfigs))
+      AwsSecretsConfigs = JSON.parse(core.getInput('eks_secrets_configs'));
     } catch (error) {
-      return core.setFailed('Invalid JSON String on eks_secrets_configs.')
+      return core.setFailed('Invalid JSON String on eks_secrets_configs.');
     }
 
-    if(!AwsSecretsConfigs.length) return core.setFailed('No config provided.');
+    if (!AwsSecretsConfigs.length) {
+      return core.setFailed('No config provided.');
+    }
 
-    const outputSecretNamespace = core.getInput('output_secret_namespace')
+    const outputSecretNamespace = core.getInput('output_secret_namespace');
     for (const config of AwsSecretsConfigs) {
-       // Connect with AWS 
+      // Connect with AWS
       const secretFromAWS = await awsHelper.getSecretsCredentialsFrom(config.secret_name, {
         accessKeyId: core.getInput('AWS_EKS_ACCESS_KEY'),
-        secretAccessKey: core.getInput('AWS_EKS_SECRET_KEY')
+        secretAccessKey: core.getInput('AWS_EKS_SECRET_KEY'),
       });
+
       const secretFormat = {
         apiVersion: 'v1',
-        kind: 'Secret', 
+        kind: 'Secret',
         metadata: {
           name: config.output_secret_name,
-          ...(outputSecretNamespace ? {namespace: outputSecretNamespace} : {} )
+          ...(outputSecretNamespace ? { namespace: outputSecretNamespace } : {}),
         },
         type: 'Opaque',
-        stringData: secretFromAWS
-      }
+        stringData: secretFromAWS,
+      };
 
-      const outputType = core.getInput('output_type'); 
-      let secretFinalOutput = null; 
-      if(outputType === 'yaml'){
+      const outputType = core.getInput('output_type');
+      let secretFinalOutput = null;
+      if (outputType === 'yaml') {
         secretFinalOutput = YAML.stringify(secretFormat);
-      }else{
+      } else {
         secretFinalOutput = JSON.stringify(secretFormat);
       }
-      
-      secretCollections.push(secretFinalOutput)
+
+      secretCollections.push(secretFinalOutput);
     }
-  
-    
-  
-    // // `who-to-greet` input defined in action metadata file
-    // const nameToGreet = core.getInput('who-to-greet');
-    // console.log(`Hello ${nameToGreet}!`);
-    // const time = (new Date()).toTimeString();
-    // core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
+
+    const finalFile = secretCollections.join('\n---\n');
+    core.setOutput('final_string', finalFile);
+
+    fs.writeFileSync(core.getInput('output_file_name'), finalFile);
   } catch (error) {
     core.setFailed(error.message);
   }
-}
+};
 
-run()
+run();
--- src/aws-helpers.js
+++ src/aws-helpers.js
@@ -1,23 +1,24 @@
-const AWS = require('aws-sdk'); 
+const AWS = require('aws-sdk');
 
+module.exports.getSecretsCredentialsFrom = async (secretName, { accessKeyId, secretAccessKey }) => {
+  if (!secretName) {
+    throw Error('Missing secret name.');
+  }
 
-module.exports.getSecretsCredentialsFrom = async (secretName, { accessKeyId, secretAccessKey }) =>{
-    if(!secretName) throw Error('Missing secret name.')
+  const SecretManagerClient = new AWS.SecretsManager({
+    region: 'us-east-1',
+    accessKeyId,
+    secretAccessKey,
+  });
 
-    const SecretManagerClient = new AWS.SecretsManager({
-        region: 'us-east-1',
-        accessKeyId,
-        secretAccessKey,
-    });
-    
-    let secretJSON; 
-    const SecretResult = await SecretManagerClient.getSecretValue({SecretId: secretName}).promise()
-    
-    if ('SecretString' in SecretResult) {
-        secretJSON = JSON.parse(SecretResult.SecretString);
-    } else {
-        let buff = new Buffer(SecretResult.SecretBinary, 'base64');
-        secretJSON = JSON.parse(buff.toString('ascii'));
-    }
-    return secretJSON; 
-}
+  let secretJSON;
+  const SecretResult = await SecretManagerClient.getSecretValue({ SecretId: secretName }).promise();
+
+  if ('SecretString' in SecretResult) {
+    secretJSON = JSON.parse(SecretResult.SecretString);
+  } else {
+    const buff = new Buffer(SecretResult.SecretBinary, 'base64');
+    secretJSON = JSON.parse(buff.toString('ascii'));
+  }
+  return secretJSON;
+};
